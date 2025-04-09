b83f751ed284746d7f432d3bda586dfbffe89661
miloul <milouw56@gmail.com>
2025-04-09 18:35:40 +0900

    feat: chart

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,24 +1,64 @@
 const API_KEY = "4e6a7276666d696c38316a50746944";
+const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/`;
 
-let data;
+const getFebSubway = async () => {
+  const res = await fetch(API_URL + "20250210/4호선");
+  const data = await res.json();
 
-let xhr = new XMLHttpRequest();
-let url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/20250331/4호선`; /* URL */
-xhr.open("GET", url);
-xhr.onreadystatechange = function () {
-  if (this.readyState == xhr.DONE) {
-    // <== 정상적으로 준비되었을때
-    if (xhr.status == 200 || xhr.status == 201) {
-      // <== 호출 상태가 정상적일때
-
-      let txt = JSON.stringify(this.response);
-      data = this.response;
-      console.log(txt);
-      document.querySelector("#responseData").innerHTML = txt;
-      console.log(txt.CardSubwayStatsNew.list_total_count);
-    }
-  }
+  return data;
 };
-xhr.send("");
 
-console.log(data);
+const result = await getFebSubway();
+
+const cnt = result.CardSubwayStatsNew.list_total_count;
+
+const graphInfo = [];
+
+for (const g of result.CardSubwayStatsNew.row) {
+  graphInfo.push({
+    name: g.SBWY_STNS_NM,
+    off: g.GTOFF_TNOPE,
+    on: g.GTON_TNOPE,
+  });
+}
+
+const DATA_COUNT = cnt;
+const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+const labels = [];
+const off = [];
+const on = [];
+graphInfo.filter((info) => {
+  labels.push(info.name);
+  off.push(info.off);
+  on.push(info.on);
+});
+console.log(labels);
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "승차",
+        data: on,
+        borderWidth: 1,
+      },
+      {
+        label: "하차",
+        data: off,
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
