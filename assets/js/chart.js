447396b419cae0cdd6c2fcf4b4bae28c14d4c92e
miloul <milouw56@gmail.com>
2025-04-10 15:22:14 +0900

    feat: graph

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,64 +1,79 @@
 const API_KEY = "4e6a7276666d696c38316a50746944";
-const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/20250210/4호선`;
+const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/`;
 
 const getFebSubway = async () => {
-  const res = await fetch(API_URL);
+  const date = document.getElementById("date").value.split("-").join("");
+  let line = document.getElementById("line");
+  let lineNum = line.options[line.selectedIndex].label;
+
+  const res = await fetch(API_URL + `${date}/${lineNum}`);
   const data = await res.json();
 
   return data;
 };
 
-const result = await getFebSubway();
+const makeChart = (labels, on, off) => {
+  let chartStatus = Chart.getChart("myChart");
+  if (chartStatus !== undefined) {
+    chartStatus.destroy();
+  }
 
-const graphInfo = [];
+  const ctx = document.getElementById("myChart");
 
-for (const g of result.CardSubwayStatsNew.row) {
-  graphInfo.push({
-    name: g.SBWY_STNS_NM,
-    off: g.GTOFF_TNOPE,
-    on: g.GTON_TNOPE,
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "승차",
+          data: on,
+          borderWidth: 1,
+        },
+        {
+          label: "하차",
+          data: off,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+      },
+    },
   });
-}
+};
 
-const labels = [];
-const off = [];
-const on = [];
-graphInfo.filter((info) => {
-  labels.push(info.name);
-  off.push(info.off);
-  on.push(info.on);
-});
+document.getElementById("search").addEventListener("click", async () => {
+  const result = await getFebSubway();
 
-const ctx = document.getElementById("myChart");
+  const graphInfo = [];
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: labels,
-    datasets: [
-      {
-        label: "승차",
-        data: on,
-        borderWidth: 1,
-      },
-      {
-        label: "하차",
-        data: off,
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-    },
-  },
+  for (const g of result.CardSubwayStatsNew.row) {
+    graphInfo.push({
+      name: g.SBWY_STNS_NM,
+      off: g.GTOFF_TNOPE,
+      on: g.GTON_TNOPE,
+    });
+  }
+
+  const labels = [];
+  const off = [];
+  const on = [];
+  graphInfo.filter((info) => {
+    labels.push(info.name);
+    off.push(info.off);
+    on.push(info.on);
+  });
+
+  makeChart(labels, on, off);
 });
