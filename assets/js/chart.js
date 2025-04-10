8fdeef5c558aee9a103202bdf11e16ed7a7fc9ba
miloul <milouw56@gmail.com>
2025-04-10 09:50:07 +0900

    docs: 코드 정리

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,16 +1,14 @@
 const API_KEY = "4e6a7276666d696c38316a50746944";
-const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/`;
+const API_URL = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/CardSubwayStatsNew/1/26/20250210/4호선`;
 
 const getFebSubway = async () => {
-  const res = await fetch(API_URL + "20250210/4호선");
+  const res = await fetch(API_URL);
   const data = await res.json();
 
   return data;
 };
 
 const result = await getFebSubway();
-
-const cnt = result.CardSubwayStatsNew.list_total_count;
 
 const graphInfo = [];
 
@@ -22,9 +20,6 @@ for (const g of result.CardSubwayStatsNew.row) {
   });
 }
 
-const DATA_COUNT = cnt;
-const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
-
 const labels = [];
 const off = [];
 const on = [];
@@ -33,7 +28,6 @@ graphInfo.filter((info) => {
   off.push(info.off);
   on.push(info.on);
 });
-console.log(labels);
 
 const ctx = document.getElementById("myChart");
 
@@ -58,6 +52,12 @@ new Chart(ctx, {
     scales: {
       y: {
         beginAtZero: true,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
       },
     },
   },
